2eafe90a8a0a54669357b1202d31414f6bbb3f26
Jordy4200 <jordyvelasco3322@gmail.com>
2024-01-18 19:38:54 -0600

    Pequeños cambios

--- funcion.js
+++ funcion.js
@@ -1,15 +1,10 @@
-// Obtener los elementos div por su id
 let bloque1 = document.getElementById("bloque1");
 let bloque2 = document.getElementById("bloque2");
-
-// Asignar un evento de mouseover al primer div
 bloque1.addEventListener("mouseover", () => {
-    // Cambiar el color de fondo del segundo div a gris
     bloque2.style.backgroundColor = "grey";
 });
 
 // Asignar un evento de mouseout al primer div
 bloque1.addEventListener("mouseout", () => {
-    // Restaurar el color de fondo del segundo div a blanco
     bloque2.style.backgroundColor = "white";
 });
